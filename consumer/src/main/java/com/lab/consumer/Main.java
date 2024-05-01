4198243e14378849c71400a70dd85b07c7c6c1f5
DannyRyd <danielrydenberg@gmail.com>
2024-05-01 12:55:02 +0200

    add menu with user input

--- consumer/src/main/java/com/lab/consumer/Main.java
+++ consumer/src/main/java/com/lab/consumer/Main.java
@@ -2,6 +2,8 @@ package com.lab.consumer;
 
 import com.lab.service.EvaluateNumber;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.ServiceLoader;
 
 public class Main {
@@ -10,10 +12,41 @@ public class Main {
     public static void main(String[] args) {
 
         ServiceLoader<EvaluateNumber> loader = ServiceLoader.load(EvaluateNumber.class);
-        System.out.println("Services loaded: " + loader.stream().count());
 
-        for (EvaluateNumber evaluateNumber : loader) {
-            System.out.println(evaluateNumber.evaluate(10));
+        var implementations = new ArrayList<EvaluateNumber>();
+        var implementationNames = new ArrayList<String>();
+
+        for (EvaluateNumber eval : loader) {
+            implementations.add(eval);
+            implementationNames.add(eval.getClass().getSimpleName());
         }
+
+        if (implementations.isEmpty()) {
+            System.out.println("No implementations found.");
+            return;
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Available implementations:");
+        for (int i = 0; i < implementationNames.size(); i++) {
+            System.out.println((i + 1) + ". " + implementationNames.get(i));
+        }
+
+        System.out.print("Choose an implementation (1-" + implementationNames.size() + "): ");
+        int choice = scanner.nextInt();
+
+        if (choice < 1 || choice > implementationNames.size()) {
+            System.out.println("Invalid choice.");
+            return;
+        }
+
+        EvaluateNumber selectedImplementation = implementations.get(choice - 1);
+
+        System.out.print("Enter a number to evaluate: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Result: " + selectedImplementation.evaluate(number));
     }
+
 }
